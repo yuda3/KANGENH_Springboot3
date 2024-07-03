717a8ec1cab5effe5b51c531357e16bc648ddb68
yuda3 <yuda3@hotmail.com>
2024-07-03 17:21:00 +0900

    springboot 3 project start

---
+++ src/main/java/com/group/love2coding/Love2CodingApplication.java
@@ -0,0 +1,13 @@
+package com.group.love2coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Love2CodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Love2CodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/love2coding/Love2CodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.love2coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Love2CodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
